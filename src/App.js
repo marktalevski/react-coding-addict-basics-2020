30539751eeec4c89468a1a382093e5fac429a3c0
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-10 23:40:57 -0500

    Removed uneeded create-react-app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import TodoList from './TodoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [todos, setTodos] = useState([])
+
+  return(
+    <>
+      <TodoList todos={todos}/>
+      <input type='text'></input>
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 Todos completed</div>
+    </>
+  )
 }
 
 export default App;
