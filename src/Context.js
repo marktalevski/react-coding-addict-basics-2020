d1770b2dfe6b177f4295e6d5f7ca64995d92dd7b
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-11 22:47:39 -0500

    Fetch API to fetch photos

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
+    
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+          .then(res => res.json())
+          .then(data => setAllPhotos(data))
+      }, [])
     
     return (
         <Context.Provider value={{allPhotos}}>
