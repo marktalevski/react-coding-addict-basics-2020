59207d11299e18df63710cdcd810c9bd49012395
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-17 13:54:24 -0400

    Practice rendering using props (prop drilling)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import './styles.css'
+
+const firstBook = {
+  image:'https://m.media-amazon.com/images/I/91SkYC1TRHL._AC_UY218_.jpg',
+  title:'The Way of Kings',
+  author:'Brandon Sanderson'
+}
+
+const secondBook = {
+  image:'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
+  title:'Beyond Chaos',
+  author:'Jordan Peterson'
+}
+
+const App = (props) => {
+  return (
+    <section 
+      className='booklist'>
+      <Book 
+        title={firstBook.title} 
+        author={firstBook.author} 
+        image={firstBook.image} />
+      <Book 
+        title={secondBook.title}
+        author={secondBook.author}
+        image={secondBook.image} />
+    </section>
+  );
+};
+
+const Book = (props) => {
+  return (
+    <article 
+      className='book'>
+      <Image
+        image={props.image} />
+      <Title 
+        title={props.title} />
+      <Author 
+        author={props.author} />
+    </article>
+  );
+}
+
+export const Image = (props) => {
+  return (
+    <article>
+        <img 
+          src={props.image}
+          alt='' />
+    </article>
+  );
+}
+
+const Title = (props) => {
+  return (
+    <h1>
+      {props.title}
+    </h1>
+  )
+}
+
+const Author = (props) => {
+  return(
+    <h4 style={{
+      color: '#617d98', 
+      fontSize: '0.75rem', 
+      marginTop: '.25rem'}}
+    >
+      {props.author}
+    </h4>
+  )  
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
