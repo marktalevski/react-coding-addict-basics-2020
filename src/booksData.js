a41c875e89ea80993f7da44561b077b97b46d1a0
Mark Talevski <mark.talevski+git@gmail.com>
2021-03-18 14:40:21 -0400

    Changed id from string to number

--- src/booksData.js
+++ src/booksData.js
@@ -1,12 +1,12 @@
 const booksData = [
    {
-      id: '1',
+      id: 1,
       image: 'https://m.media-amazon.com/images/I/91SkYC1TRHL._AC_UY218_.jpg',
       title: 'The Way of Kings',
       author: 'Brandon Sanderson',
    },
    {
-   id: '2',
+   id: 2,
       image:
       'https://images-na.ssl-images-amazon.com/images/I/71xLmdLOQ0L._AC_UL200_SR200,200_.jpg',
       title: 'Beyond Chaos',
